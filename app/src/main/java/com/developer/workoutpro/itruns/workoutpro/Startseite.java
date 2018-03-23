90dcfcedb528abd4cb2aefb4f746cf3617ace3be
Jan <jan.bohnenkamp@paderborn.com>
2018-03-23 23:53:49 +0100

    Initial commit

---
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/Startseite.java
@@ -0,0 +1,13 @@
+package com.developer.workoutpro.itruns.workoutpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Startseite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startseite);
+    }
+}
