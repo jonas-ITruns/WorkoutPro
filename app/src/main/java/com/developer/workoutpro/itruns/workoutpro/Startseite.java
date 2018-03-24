8af25b7dc01304f51c3d266ce2d061115c0b73a0
Jonas Brand <jonasbrand@paderborn.com>
2018-03-24 20:57:59 +0100

    Nice

--- app/src/main/java/com/developer/workoutpro/itruns/workoutpro/Startseite.java
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/Startseite.java
@@ -1,14 +1,51 @@
 package com.developer.workoutpro.itruns.workoutpro;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Startseite extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ImageButton menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startseite);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+
+                    }
+                });
+
+        menuButton = findViewById(R.id.imgbtnMenue);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
 
 
