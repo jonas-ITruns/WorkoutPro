02e0355835b7ca229dea7265a559eb817e12e4bd
Jan <jan.bohnenkamp@paderborn.com>
2018-03-26 00:02:46 +0200

    Geile Scheiße

---
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/WelcomeScreen.java
@@ -0,0 +1,26 @@
+package com.developer.workoutpro.itruns.workoutpro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity{
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(WelcomeScreen.this, MainClass.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+    }
+}
