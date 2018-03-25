7ae9d794f1ef6e9fcf7864605468ff8ea2b6e3b0
Jonas Brand <jonasbrand@paderborn.com>
2018-03-25 01:29:21 +0100

    Nice

--- app/src/main/java/com/developer/workoutpro/itruns/workoutpro/Startseite.java
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/Startseite.java
@@ -1,11 +1,20 @@
 package com.developer.workoutpro.itruns.workoutpro;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.constraint.solver.widgets.ConstraintTableLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuItemImpl;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -14,6 +23,7 @@ public class Startseite extends AppCompatActivity {
     // Menüleiste:
     private DrawerLayout mDrawerLayout;
     private ImageButton menuButton;
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +36,34 @@ public class Startseite extends AppCompatActivity {
     public void menueleiste() {
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-
                         menuItem.setChecked(true);
+                        switch(menuItem.getItemId()) {
+                            case R.id.overview:
+                                setContentView(R.layout.activity_startseite);
+                                break;
+                            case R.id.exercises:
+                                setContentView(R.layout.activity_uebungen_uebersicht);
+                                break;
+                            case R.id.premium:
+                                setContentView(R.layout.activity_premium);
+                                break;
+                            case R.id.support:
+                                setContentView(R.layout.activity_support);
+                                break;
+                            case R.id.settings:
+                                setContentView(R.layout.activity_settings);
+                                break;
+                        } // switch
 
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
+                        menueleiste();
+                        mDrawerLayout.openDrawer(GravityCompat.START);
 
                         return true;
-
                     }
                 });
 
