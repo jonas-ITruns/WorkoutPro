828472c8ecb4e90a1cfee2ce8f2baf5fe26f4c42
Jonas Brand <jonasbrand@paderborn.com>
2018-03-27 19:44:55 +0200

    Nice

--- app/src/main/java/com/developer/workoutpro/itruns/workoutpro/WelcomeScreen.java
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/WelcomeScreen.java
@@ -1,21 +1,35 @@
 package com.developer.workoutpro.itruns.workoutpro;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 
 public class WelcomeScreen extends AppCompatActivity{
 
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int delay = 5000;
+    private static int SPLASH_TIME_OUT = delay;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(delay);
+        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, delay); // see this max value coming back here, we animate towards that value
+        animation.setDuration (delay); //in milliseconds
+        animation.setInterpolator (new DecelerateInterpolator());
+        animation.start ();
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
+                progressBar.clearAnimation();
                 Intent homeIntent = new Intent(WelcomeScreen.this, MainClass.class);
                 startActivity(homeIntent);
                 finish();
@@ -23,4 +37,5 @@ public class WelcomeScreen extends AppCompatActivity{
 
         },SPLASH_TIME_OUT);
     }
+
 }
