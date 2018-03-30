cd3bfcd488b2280d93aeef849dab0ca166be6ce4
Jan <jan.bohnenkamp@paderborn.com>
2018-03-30 21:01:30 +0200

    Geile Scheiße

---
+++ app/src/main/java/com/developer/workoutpro/itruns/workoutpro/FrMeineUebungenVideo.java
@@ -0,0 +1,22 @@
+package com.developer.workoutpro.itruns.workoutpro;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FrMeineUebungenVideo extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fr_meine_uebungen_video, container, false);
+
+        return view;
+    }
+
+}
